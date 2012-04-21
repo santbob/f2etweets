852d58751176a9103016a2142697cad3708855f7
Santhosh Nageshwar <santhoshbabun@gmail.com>
2012-04-21 15:59:08 -0700

    dsf

--- server.js
+++ server.js
@@ -4,7 +4,7 @@
  */
 
 var express = require('express')
-  , routes = require('./routes'), twit = require('./helper/twitter');
+  , routes = require('./routes'), twit = require('./helper/twit');
 
 var server = module.exports = express.createServer();
 var io = require('socket.io').listen(server);
@@ -26,22 +26,47 @@ server.configure('development', function(){
 server.configure('production', function(){
   server.use(express.errorHandler());
 });
+var jsQuery, cssQuery, htmlQuery, search;
+search = function (query){
+        twit.search(query, function(err, data) {
+              if(err){
+                console.log("Some error - "+err);
+                return false;
+              }
+              if(data){
+                return data.results;
+              }
+        });
+};
+
+jsQuery = function (){
+        var query = 'js OR javascript OR ecmascript from:addy_osmani OR from:badass_js OR from:smashingmag OR from:jsprf OR from:__DavidFlanagan OR from:slicknet OR from:jeresig';
+        return search(query);
+};
+
+htmlQuery = function (){
+        var query = 'html OR html5 from:slicknet OR from:thierrykoblentz';
+        return search(query);
+};
+
+cssQuery = function (){
+        var query = 'css OR css3 from:slicknet OR from:thierrykoblentz';
+        return search(query);
+};
 
 // Routes
-
 server.get('/', routes.index);
 server.get('/home', function(req, res){
         res.render('home', {
         title: 'Home',
         data:{
-            'html': twit.htmlQuery(),
-            'js': twit.jsQuery(),
-            'css': twit.cssQuery()
+            'html': htmlQuery(),
+            'js': jsQuery(),
+            'css': cssQuery()
         }
     });
 });
 server.listen(3000, function(){
   console.log("Express server listening on port %d in %s mode", server.address().port, server.settings.env);
 });
-
 
